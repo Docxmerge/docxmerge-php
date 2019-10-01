e41aede4cd2dfbb8c214ef29e258e7c593038480
dviejo <dvp123456@gmail.com>
2019-10-01 14:49:50 +0200

    new api

--- src/Docxmerge.php
+++ src/Docxmerge.php
@@ -9,190 +9,107 @@ class Docxmerge
     private $baseUrl;
     private $tenant;
 
-    public function __construct($apiKey, $baseUrl, $tenant = "default")
+    public function __construct($apiKey, $tenant = "default", $baseUrl = "https://api.docxmerge.com")
     {
         $this->apiKey = $apiKey;
         $this->baseUrl = $baseUrl;
         $this->tenant = $tenant;
     }
 
-    public function transformTemplate($fp, $templateName)
-    {
-        $baseUrl = $this->baseUrl;
-        $ch = curl_init("$baseUrl/api/v1/Admin/TransformTemplate?template=$templateName");
-        curl_setopt($ch, CURLOPT_FILE, $fp);
-        $this->hydrateCurl($ch);
-
-        curl_exec($ch);
-        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-        if ($http_code != "200") throw new \Exception("Unexpected status code $http_code");
-    }
-
-    public function transformDocument($fp, $docxPath)
-    {
-        $baseUrl = $this->baseUrl;
-        $ch = curl_init("$baseUrl/api/v1/Admin/TransformFile");
-        curl_setopt($ch, CURLOPT_FILE, $fp);
-        $this->hydrateCurl($ch);
-        $fields = [
-            'file' => new \CurlFile($docxPath, '', 'file.docx'),
-        ];
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-        curl_exec($ch);
-        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-        if ($http_code != "200") throw new \Exception("Unexpected status code $http_code");
-    }
-
-    public function mergeTemplate($fp, $templateName, $data)
+    public function renderTemplate($fp, $templateName, $data, $conversionType, $version = "")
     {
         $baseUrl = $this->baseUrl;
         $apiKey = $this->apiKey;
         $tenant = $this->tenant;
 
-        $ch = curl_init("$baseUrl/api/v1/Admin/MergeTemplate?template=$templateName");
+        $ch = curl_init("$baseUrl/api/v1/Admin/RenderTemplate");
 
-        $payload = json_encode($data);
-
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_FILE, $fp);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        $body_len = strlen($payload);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                'Content-Type: application/json',
-                "Content-Length:$body_len",
-                "api-key: $apiKey",
-                "x-tenant: $tenant",
-            )
-        );
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-
-        curl_exec($ch);
-        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-        if ($http_code != "200") throw new \Exception("Unexpected status code $http_code");
-    }
-
-    public function mergeDocument($fp, $docxPath, $data)
-    {
-        $baseUrl = $this->baseUrl;
-        $apiKey = $this->apiKey;
-        $tenant = $this->tenant;
-
-        $ch = curl_init("$baseUrl/api/v1/Admin/MergeFile");
-
-        $payload = json_encode($data);
-
-        $options = array(
-            CURLOPT_POST => true,
-            CURLOPT_HTTPHEADER => array(
-                "api-key: $apiKey",
-                "x-tenant: $tenant",
-            ),
-            CURLOPT_POSTFIELDS => array(
-                'file' => new \CurlFile("$docxPath", '', 'file'),
-                'data' => $payload,
-            ),
-            CURLOPT_FILE => $fp,
-        );
-        curl_setopt_array($ch, $options);
-        curl_exec($ch);
-        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-        if ($http_code != "200") throw new \Exception("Unexpected status code $http_code");
-    }
-
-    public function mergeAndTransformTemplate($fp, $templateName, $data)
-    {
-        $baseUrl = $this->baseUrl;
-        $apiKey = $this->apiKey;
-        $tenant = $this->tenant;
-
-        $ch = curl_init("$baseUrl/api/v1/Admin/MergeAndTransformTemplatePost?template=$templateName");
-
-        $payload = json_encode($data);
-
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_FILE, $fp);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        $body_len = strlen($payload);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                'Content-Type: application/json',
-                "Content-Length:$body_len",
-                "api-key: $apiKey",
-                "x-tenant: $tenant",
-            )
-        );
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-
-        curl_exec($ch);
-        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-        if ($http_code != "200") throw new \Exception("Unexpected status code $http_code");
-    }
-
-    public function mergeAndTransformDocument($fp, $docxPath, $data)
-    {
-        $baseUrl = $this->baseUrl;
-        $apiKey = $this->apiKey;
-        $tenant = $this->tenant;
-
-        $ch = curl_init("$baseUrl/api/v1/Admin/MergeAndTransform");
-
-        $payload = json_encode($data);
-
-        $options = array(
-            CURLOPT_POST => true,
-            CURLOPT_HTTPHEADER => array(
-                "api-key: $apiKey",
-                "x-tenant: $tenant",
-            ),
-            CURLOPT_POSTFIELDS => array(
-                'file' => new \CurlFile("$docxPath", '', 'file'),
-                'data' => $payload,
-            ),
-            CURLOPT_FILE => $fp,
-        );
-        curl_setopt_array($ch, $options);
-        curl_exec($ch);
-        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-        if ($http_code != "200") throw new \Exception("Unexpected status code $http_code");
-    }
-
-
-    private function hydrateCurl($ch)
-    {
-        $apiKey = $this->apiKey;
-        $tenant = $this->tenant;
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "api-key: $apiKey",
-            "x-tenant: $tenant",
+        $payload = json_encode(array(
+            "data" => $data,
+            "template" => $templateName,
+            "conversionType" => $conversionType,
+            "version" => $version,
         ));
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        $body_len = strlen($payload);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                "Content-Length:$body_len",
+                "api-key: $apiKey",
+                "x-tenant: $tenant",
+            )
+        );
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+
+        curl_exec($ch);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        if ($http_code != "200") throw new \Exception("Unexpected status code $http_code");
     }
 
-    /**
-     * @return mixed
-     */
-    public function getBaseUrl()
+    public function renderUrl($fp, $url, $data, $conversionType)
     {
-        return $this->baseUrl;
+        $baseUrl = $this->baseUrl;
+        $apiKey = $this->apiKey;
+        $tenant = $this->tenant;
+
+        $ch = curl_init("$baseUrl/api/v1/Admin/RenderUrl");
+
+        $payload = json_encode(array(
+            "data" => $data,
+            "url" => $url,
+            "conversionType" => $conversionType,
+        ));
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        $body_len = strlen($payload);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                "Content-Length:$body_len",
+                "api-key: $apiKey",
+                "x-tenant: $tenant",
+            )
+        );
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+
+        curl_exec($ch);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        if ($http_code != "200") throw new \Exception("Unexpected status code $http_code");
     }
 
-    /**
-     * @return mixed
-     */
-    public function getApiKey()
+    public function renderFile($fp, $docxPath, $data, $conversionType)
     {
-        return $this->apiKey;
-    }
+        $baseUrl = $this->baseUrl;
+        $apiKey = $this->apiKey;
+        $tenant = $this->tenant;
 
-    /**
-     * @return string
-     */
-    public function getTenant(): string
-    {
-        return $this->tenant;
+        $ch = curl_init("$baseUrl/api/v1/Admin/RenderFile");
+
+        $payload = json_encode($data);
+
+        $options = array(
+            CURLOPT_POST => true,
+            CURLOPT_HTTPHEADER => array(
+                "api-key: $apiKey",
+                "x-tenant: $tenant",
+            ),
+            CURLOPT_POSTFIELDS => array(
+                'file' => new \CurlFile("$docxPath", '', 'file'),
+                'data' => $payload,
+                'conversionType' => $conversionType
+            ),
+            CURLOPT_FILE => $fp,
+        );
+        curl_setopt_array($ch, $options);
+        curl_exec($ch);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        if ($http_code != "200") throw new \Exception("Unexpected status code $http_code");
     }
 }
